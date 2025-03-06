2c015a8103a367d53f47b9a66ad2ad6582fabeba
DashaIgnatieva <dariyignatieva@mail.ru>
2025-03-06 19:36:51 +0300

    added a channel definitions in the messages container

--- frontend/src/Components/MessagesContainer.jsx
+++ frontend/src/Components/MessagesContainer.jsx
@@ -15,7 +15,7 @@ const MessagesContainer = () => {
             <div className="d-flex flex-column h-100">
                 <div className="bg-light mb-4 p-3 shadow-sm small">
                     <p className="m-0">
-                        <b># general</b>
+                        <b># {activeChannel.name}</b>
                     </p>
                     <span className="text-muted">{count} сообщений</span>
                 </div>
